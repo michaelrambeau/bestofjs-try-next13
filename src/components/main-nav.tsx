20d502358c31d159741241a803f98d67ed6c51ff
michaelrambeau <mikeair@gmail.com>
2023-07-02 16:03:53 +0900

    Fix missing type

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -6,10 +6,17 @@ import * as React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-import { NavItem } from "@/types/nav";
 import { siteConfig } from "@/config/site";
 import { cn } from "@/lib/utils";
 import { Icons } from "@/components/icons";
+
+export interface NavItem {
+  title: string;
+  href?: string;
+  disabled?: boolean;
+  external?: boolean;
+  isActive: (pathname: string) => boolean;
+}
 
 const mainNavItems: NavItem[] = [
   {
--- types/nav.ts
+++
@@ -1,7 +0,0 @@
-export interface NavItem {
-  title: string;
-  href?: string;
-  disabled?: boolean;
-  external?: boolean;
-  isActive: (pathname: string) => boolean;
-}
